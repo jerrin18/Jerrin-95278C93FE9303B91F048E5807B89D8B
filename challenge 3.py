e9330f44ddeccf52db359e412df2cdbda4b20807
jerrin18 <144418385+jerrin18@users.noreply.github.com>
2023-09-19 21:09:02 +0530

    Add files via upload

---
+++ challenge 3.py
@@ -0,0 +1,35 @@
+#Bank Account
+
+class BankAccount:
+	
+	def __init__(self , account_number , account_holder_name , initial_balance=0.0):
+		self.__account_number = account_number
+		self.__account_holder_name = account_holder_name
+		self.__account_balance = initial_balance
+		
+	def deposit(self , amount):
+		if amount > 0:
+			self.__account_balance += amount
+			print("Deposited ₹{}. New balance: ₹{}" . format(amount, self.__account_balance))
+			
+		else:
+			print("Invalid deposit amount. Please deposit a positive amount.")
+			
+	def withdraw(self , amount):
+		if amount > 0 and amount <= self.__account_balance:
+			self.__account_balance -= amount
+			print("Withdrew ₹{}. New balance: ₹{}" .format(amount , self.__account_balance))
+			
+		else:
+			print("Invalid withdrawal amount or insufficient balance.")
+			
+	def display_balance(self):
+		  print("Account balance for {} (Account #{}): ₹{}" .format(
+		self.__account_holder_name, self.__account_number, self.__account_balance))
+		
+account= BankAccount(account_number="123556789",account_holder_name="ABC", initial_balance=10000.0)
+		      
+account.display_balance()
+account.deposit(600.0)
+account.withdraw(100.0)
+account.display_balance()
